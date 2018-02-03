b705ef0491226ca5d49e2cc5475b100d1307a03f
Trent <mr.peschke@gmail.com>
2018-02-03 13:15:27 -0700

    resurrect debugging

--- src/playerview/Playerview.js
+++ src/playerview/Playerview.js
@@ -82,6 +82,7 @@ export default class PlayerView extends Component {
             }
         })
         this.socket.on(`${battle}-resurrect`, data => {
+            console.log('player')
             if (data.id) {
                 var topfighter = this.state.fighterList.map(val => {
                     if (val.id === data.id) {
