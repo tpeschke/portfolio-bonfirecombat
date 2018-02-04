45c9b1224fbdd1d2872829576420882db4da6ad4
Trent <mr.peschke@gmail.com>
2018-02-04 09:54:22 -0700

    fixing home screen

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -11,18 +11,18 @@ export default class Home extends Component {
             <div className="outHome" id="home">
 
                 <div className="foreground">
-                    {/* <div className="videoBox">
-                        <iframe src="https://www.youtube.com/embed/3nR_KGnaSD4?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1&loop=1&mute=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                        <div className="coverUp"></div>
-                    </div> */}
+                    <div className="videoBox">
+                        {/* <iframe src="https://www.youtube.com/embed/3nR_KGnaSD4?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1&loop=1&mute=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> */}
+                        {/* <div className="coverUp"></div>  */}
+                    </div>
 
-                    <div className="outDiveHome">
+                    {/* <div className="outDiveHome"> */}
                         <div className="logoHome">
                             <h4 className="logoWords">Combat Counter</h4></div>
 
                         <SignIn />
 
-                    </div>
+                    {/* </div> */}
                 </div>
 
             </div>
