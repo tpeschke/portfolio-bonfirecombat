35db9c5c4cdfcf920319467ff0bc475e87912259
Trent <mr.peschke@gmail.com>
2018-11-02 11:28:35 -0600

    removed some console logs

--- src/components/sort.js
+++ src/components/sort.js
@@ -5,10 +5,8 @@ export default function sort(fightArr, count) {
 
         fightArr.forEach(val => {
             if (val.actioncount > count) {
-                console.log(val.namefighter)
                 val.acting = '1'
             } else {
-                console.log(val.namefighter)
                 val.acting = '0'
                 val.topcheck = '0'
             }
