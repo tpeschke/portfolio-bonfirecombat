dd0354572136f950a683c66a35111a47a61f833b
Trent <mr.peschke@gmail.com>
2018-12-10 15:21:45 -0700

    cleared some stuff up

--- src/Home/SignIn.js
+++ src/Home/SignIn.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 export default class SignIn extends Component {
-
     render() {
-
         return (
             <div className='signIn'>
                 <div className='signInInner'>
--- src/components/diceRoll.js
+++ src/components/diceRoll.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function rollDice(sides) {
     return +Math.floor(Math.random() * Math.floor(sides)) + 1
 }
