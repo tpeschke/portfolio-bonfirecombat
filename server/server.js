656adf3e9524ea8d19836ab6282b4046b88a7404
Trent <mr.peschke@gmail.com>
2019-08-25 20:54:14 -0600

    redid the sign in screen

--- server/server.js
+++ server/server.js
@@ -51,15 +51,12 @@ passport.use(new Auth0Strategy({
 ////TESTING TOPLEVEL MIDDLEWARE////
 ///COMMENET OUT WHEN AUTH0 READY///
 ///////////////////////////////////
-// app.use((req, res, next) =>{
-//     if(!req.session.user){
-//         req.session.user = {
-//             user_id: 1,
-//             user_name: "harrison ford", 
-//             email: "adventureBuilder2049@gmail.com", 
-//             name: "adventure", 
-//             profile_picture : "http://www.placekitten.com/200/250",
-//             auth_id: "adsgfhaoibjmoi5wrhgiuaosfngiuasdhg;ioarhdgv;ou"
+// app.use((req, res, next) => {
+//     if (!req.user) {
+//         req.user = {
+//             id: 1,
+//             email: "mr.peschke@gmail.com",
+//             patreon: 1
 //         }
 //     }
 //     next();
@@ -116,7 +113,7 @@ const path = require('path')
 
 // ==========================================
 
-const port = process.env.PORT
+const port = process.env.SERVER_PORT
 
 massive(process.env.CONNECTION_STRING).then(dbInstance => {
     app.set('db', dbInstance);
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,8 +2,6 @@ import React from 'react'
 import './Home.css'
 import axios from 'axios'
 
-import SignIn from './SignIn'
-
 export default function Home(props) {
     axios.get('/auth/me').then(v => {
         if (v.status === 200) {
@@ -13,9 +11,16 @@ export default function Home(props) {
 
     return (
         <div className="outHome" id="home">
+            <div className='logoHome'></div>
+            <div className="topBorder">
+                <h4 className="logoWords">Combat Counter</h4>
+            </div>
             <div className="foreground">
-                <div className='logoHome'></div>
-                <SignIn/>
+                <p className='signInText'>For the <a class="bonfireLink" href="https://bonfire.dragon-slayer.net/">Bonfire Roleplaying System.</a></p>
+                <a class="bonfireLink" id="signInButton" href={`${process.env.REACT_APP_LOGIN}`}>
+                    Sign In Here
+                    </a>
+                <p className='signInText'>with your Google, FaceBook, Twitter, or Yahoo account</p>
             </div>
         </div>
     )
